84049ca1b7aa157577bd9839c1c41aaa1a6d0b5f
cristhian075 <62954277+cristhian075@users.noreply.github.com>
2021-04-16 08:00:16 -0500

    Actualización

--- src/index.js
+++ src/index.js
@@ -3,56 +3,56 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
 // inicio
-const app =express();
+const app = express();
 
 //configuracion
-app.set('port',process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
-app.set('views',path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.engine('.hbs', exphbs({
-    defaultLayout:'main',
-    layoutDir: path.join(app.get('views'),'layouts'),
-    partialsDir: path.join(app.get('views'),'partials'),
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
 }));
-app.set('view engine','.hbs');
+app.set('view engine', '.hbs');
 
 
-app.set('json spaces',2);
+app.set('json spaces', 2);
 
 
 //MIDDELWARE - peticiones
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
 //variables globales
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
     next();
 });
 
 //rutas
 app.use(require('./routes'));
 app.use(require('./routes/authentication'));
-app.use('/usuarios',require('./routes/usuarios'));
-app.use('/diezmos',require('./routes/diezmos'));
-app.use('/barrios',require('./routes/barrios'));+
-app.use('/ciudades',require('./routes/ciudades'));
-app.use('/roles',require('./routes/roles'));
-app.use('/marcas',require('./routes/marcas'));
-app.use('/servicios',require('./routes/servicios'));
-app.use('/categorias',require('./routes/categorias'));
-app.use('/productos',require('./routes/productos'));
-app.use('/pedidos',require('./routes/pedidos'));
-app.use('/facturas',require('./routes/facturas'));
+app.use('/usuarios', require('./routes/usuarios/usuarios'));
+app.use('/diezmos', require('./routes/diezmos'));
+app.use('/barrios', require('./routes/barrios/barrios'));
+app.use('/ciudades', require('./routes/ciudades/ciudades'));
+app.use('/roles', require('./routes/roles/roles'));
+app.use('/marcas', require('./routes/marcas/marcas'));
+app.use('/servicios', require('./routes/servicios/servicios'));
+app.use('/categorias', require('./routes/categorias/categorias'));
+app.use('/productos', require('./routes/productos/productos'));
+app.use('/pedidos', require('./routes/pedidos/pedidos'));
+app.use('/facturas', require('./routes/facturas/facturas'));
 
 // public - navegador puede acceder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //iniciar servidor
-app.listen(app.get('port'),()=>{
-    console.log('server on port',app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
 });
